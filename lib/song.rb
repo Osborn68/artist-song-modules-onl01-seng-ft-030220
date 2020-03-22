17e0d0fa71fac30147409010d889588a2a2bd973
Christopher Osborn <z68ozz@gmail.com>
2020-03-22 22:58:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,7 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
@@ -13,9 +14,9 @@ class Song
 
   @@songs = []
 
-  def initialize
-    self.class.all << self
-  end
+  #def initialize
+    #self.class.all << self
+ # end
 
   #def self.find_by_name(name)
    # @@songs.detect{|a| a.name == name}
