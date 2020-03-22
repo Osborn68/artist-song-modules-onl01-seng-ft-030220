70a13fd5fefd2482ffc605db8482248dd2f12204
Christopher Osborn <z68ozz@gmail.com>
2020-03-22 22:18:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,13 @@
 require 'pry'
 
 class Song
+ 
+  extend Memorable::ClassMethods
+  
   attr_accessor :name
   attr_reader :artist
+  
+
 
   @@songs = []
 
@@ -18,13 +23,13 @@ class Song
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  #def self.reset_all
+  #  self.all.clear
+ #end
 
-  def self.count
-    self.all.count
-  end
+ # def self.count
+   # self.all.count
+ # end
 
   def artist=(artist)
     @artist = artist
