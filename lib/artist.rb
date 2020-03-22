c8b66823f978333f46367ca295d8f07214a0a6ec
Christopher Osborn <z68ozz@gmail.com>
2020-03-22 22:58:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ class Artist
  extend Memorable::ClassMethods
  extend Findable::ClassMethods
  include Paramable::InstanceMethods
+ include Memorable::InstanceMethods
  
  attr_accessor :name
   attr_reader :songs
@@ -13,7 +14,7 @@ class Artist
   @@artists = []
 
   def initialize
-    self.class.all << self
+    super
     @songs = []
   end
 
